945123e8977fb3c13b0d028ef2f723b9e24e99fe
Nikola <pepy90aa@gmail.com>
2025-12-04 14:40:21 +0100

    fixing types part-2

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo, useState } from 'react'
+import { useCallback, useMemo, useState } from 'react'
 import {
   ADD_BUTTON,
   EMPTY_STATE,
--- frontend/src/pages/PendingPage.tsx
+++ frontend/src/pages/PendingPage.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react'
+import { useMemo, useState } from 'react'
 import { layoutClasses, SORT_OPTIONS } from '../assets/dummy'
 import { Clock, Filter, ListCheck, Plus } from 'lucide-react'
 import { useOutletContext } from 'react-router-dom'
